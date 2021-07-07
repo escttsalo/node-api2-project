200bac72a8e1f06bf2be934ad824f2e023eaf5ac
escttsalo <escttsalo@gmail.com>
2021-07-07 03:38:25 -0600

    project setup and first endpoint in posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,18 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', async (req,res) => {
+    const posts = await Post.find()
+        try {
+            res.status(200).json(posts)
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error retrieving posts'
+            })
+        }
+})
+
+module.exports = router;
