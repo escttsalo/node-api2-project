9fd765e59b41bfb29179cd0e51ee6d0e6786a9c7
escttsalo <escttsalo@gmail.com>
2021-07-07 04:06:46 -0600

    last get and post enpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -7,12 +7,50 @@ const router = express.Router();
 router.get('/', async (req,res) => {
     const posts = await Post.find()
         try {
-            res.status(200).json(posts)
+            res.status(200).json(posts);
         } catch (err) {
             res.status(500).json({
                 message: 'Error retrieving posts'
-            })
+            });
         }
-})
+});
+
+router.get('/:id', async (req,res) => {
+    const post = await Post.findById(req.params.id);
+        try{
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({message: 'Does not exist'})
+            }
+        } catch(err) {
+            res.status(500).json({message: 'Error retrieving post'});
+        }
+});
+
+router.post('/', async (req,res) => {
+    const { title, contents } = req.body
+    if (!title || !contents ){
+        res.status(400).json({
+            message: 'A title and content are required for a post'
+        })
+    } else {
+        Post.insert(req.body)
+            .then(({id}) => {
+                return Post.findById(id)
+            })
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error saving post to database",
+                    error: err.message,
+                })
+            })
+    }
+});
+
+
 
 module.exports = router;
